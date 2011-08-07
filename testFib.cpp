91c132de9b316f94c0d77a8c4d5bd1ce397c4aa6
Rony Shapiro <ronys@acm.org>
2011-08-07 14:06:54 +0300

    Added test for fib(2) - passes (\!)

--- testFib.cpp
+++ testFib.cpp
@@ -5,7 +5,7 @@
 
 TEST(FiboTest, ftest) {
   struct {int n; int fn;} cases[] = {
-    {0, 0}, {1, 1},
+    {0, 0}, {1, 1}, {2, 1},
   };
   for (int i = 0; i < sizeof(cases)/sizeof(cases[0]); i++)
     EXPECT_EQ(cases[i].fn, fib(cases[i].n));
